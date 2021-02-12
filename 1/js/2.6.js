164f37372f55584cfce7a07cb0de5945887f0865
VitalieVSV <dut66AHJCT>
2021-02-12 12:23:18 +0200

    2.6

---
+++ 1/js/2.6.js
@@ -0,0 +1,11 @@
+const number = +prompt("give number");
+
+if (number > 0) {
+    console.log(1)
+} else if (number < 0) {
+    console.log(-1)
+} else if (number === 0) {
+    console.log(0)
+} else {
+    console.log("Error");
+}
