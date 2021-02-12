ed6ba25369b74bace60038398955522564af5135
VitalieVSV <dut66AHJCT>
2021-02-12 13:56:17 +0200

    3.3

--- 1/3/3.3.js
+++ 1/3/3.3.js
@@ -2,8 +2,9 @@ const styles = ["Jazz", "Bliuz"];
 
 styles.push("Rock-n-Roll");
 console.log(styles);
-const middle = styles[Math.floor(styles.length / 2)];
-styles[middle] = "Classic";
+const middle = Math.floor(styles.length / 2);
+styles[middle] = "Classic"
+console.log(styles);
 console.log(styles.shift());
 console.log(styles);
 styles.unshift("Rap", "Reggi");
