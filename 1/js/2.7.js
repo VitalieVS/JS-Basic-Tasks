6e3a79c19356729fc93ee6c20d7d30e8e40cb986
VitalieVSV <dut66AHJCT>
2021-02-12 12:38:48 +0200

    2.7

---
+++ 1/js/2.7.js
@@ -0,0 +1,13 @@
+const age = 12;
+
+if (age <= 14 === 90 <= age) {
+    console.log("Se afla in interval")
+} else {
+    console.log("Nu se afla in interval");
+}
+
+if (!(age <= 14 === 90 <= age)) {
+    console.log("Nu se afla in interval");
+} else {
+    console.log("Se afla in interval")
+}
