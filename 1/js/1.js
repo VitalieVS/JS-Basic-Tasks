89cef0fbdf7ea6dae089d234b341c41e06bca563
VitalieVSV <dut66AHJCT>
2021-02-12 12:08:44 +0200

    Initial

---
+++ 1/js/1.js
@@ -0,0 +1,34 @@
+const number = 2;
+
+switch (number) {
+    case 1:
+        console.log("Unu");
+        break;
+    case 2:
+        console.log("doi");
+        break;
+    case 3:
+        console.log("tri");
+        break;
+    case 4:
+        console.log("patr");
+        break;
+    case 5:
+        console.log("cinc");
+        break;
+    case 6:
+        console.log("sas");
+        break;
+    case 7:
+        console.log("sapt");
+        break;
+    case 8:
+        console.log("opt");
+        break;
+    case 9:
+        console.log("nou");
+        break;
+
+    default:
+        console.log("unknown number");
+}
