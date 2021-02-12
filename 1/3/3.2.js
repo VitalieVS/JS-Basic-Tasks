b867555cfb5dc7968ea7d8fec1667d2f4dd519cb
VitalieVSV <dut66AHJCT>
2021-02-12 13:17:04 +0200

    code optimised

--- 1/3/3.2.js
+++ 1/3/3.2.js
@@ -1,48 +1,45 @@
 const data = [
     {
         name: "Tudor",
-        marks: [2,4,2,4,7]
+        marks: [2, 4, 2, 4, 7]
     },
     {
         name: "Marinela",
-        marks: [2,3,4,5,6]
+        marks: [2, 3, 4, 5, 6]
     },
     {
         name: "Andrei",
-        marks: [2,2,2,2,2]
+        marks: [2, 2, 2, 2, 2]
     },
     {
         name: "Ion",
-        marks: [10,7,6,4,3,3]
+        marks: [10, 7, 6, 4, 3, 3]
     },
     {
         name: "Andriana",
-        marks: [5,5,5,5,5,5]
+        marks: [5, 5, 5, 5, 5, 5]
     }
 ];
 
 
 const averageMark = () => {
-   for (const value of data) {
-       console.log(`Numele: ${value.name}`);
-       let average = 0;
-       for (const mark of value.marks) {
-           average += mark;
-       }
-       console.log(`Average Mark: ${average / value.marks.length}`)
-   }
+    for (const value of data) {
+        console.log(`Numele: ${value.name}`);
+        let average = value.marks.reduce((a, b) => {
+            return a + b;
+        }, 0);
+        console.log(`Average Mark: ${average / value.marks.length}`)
+    }
 };
 
 const showLowAverage = () => {
     for (const value of data) {
-        let average = 0;
-        for (const mark of value.marks) {
-            average += mark;
-        }
+        let average = value.marks.reduce((a, b) => {
+            return a + b;
+        }, 0);
         if (average / value.marks.length < 5) {
             console.log(value.name);
         }
-
     }
 };
 
@@ -50,3 +47,10 @@ averageMark();
 console.log("/////////");
 showLowAverage();
 console.log("/////////");
+// with max average, or with max marks?
+console.log("/////////");
+
+
+const getAverage = array => {
+    // return Math.
+};
