60b64a2732bee29be88fa200a8986bb51fa4b09d
VitalieVSV <dut66AHJCT>
2021-02-13 18:36:59 +0200

    3.5

---
+++ 1/3/3.5.js
@@ -0,0 +1,13 @@
+const square = value => Math.pow(2, value);
+
+const convert = (square, array)  => {
+    const arr = [];
+    for (const value of array) {
+        arr.push(square(value));
+    }
+    return arr;
+};
+
+let arr = [1, 2, 3];
+console.log(convert(square, arr)); // [1, 4, 9]
+console.log(arr); // [1, 2, 3]
