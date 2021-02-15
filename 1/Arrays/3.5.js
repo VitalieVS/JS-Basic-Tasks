f6f5b088b09a7ef83253b239ba915023cb5e05a3
VitalieVSV <dut66AHJCT>
2021-02-15 15:24:05 +0200

    fixes

--- 1/Arrays/3.5.js
+++ 1/Arrays/3.5.js
@@ -1,11 +1,7 @@
 square = value => Math.pow(2, value);
 
 convert = (square, array)  => {
-    const arr = [];
-    for (const value of array) {
-        arr.push(square(value));
-    }
-    return arr;
+   return array.map(square)
 };
 
 let arr = [1, 2, 3];
