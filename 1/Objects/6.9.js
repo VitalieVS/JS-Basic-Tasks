7ac198cfa0c13084f41c537c609119f694fe218f
VitalieVSV <dut66AHJCT>
2021-02-16 18:25:50 +0200

    6.9

---
+++ 1/Objects/6.9.js
@@ -0,0 +1,18 @@
+const weekDays = {
+    "Luni": "Mo",
+    "Marti": "Tu",
+    "Miercuri": "We",
+    "Joi": "Th",
+    "Vineri": "Fr",
+    "Simbata": "Sa",
+    "Duminica": "Su"
+};
+
+swap = obj => {
+    return Object.keys(obj).reduce((ret, key) => {
+        ret[obj[key]] = key;
+        return ret;
+    }, {});
+};
+
+console.log(swap(weekDays));
