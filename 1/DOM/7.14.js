69248e2e40e6a203478285d57233c444d580e283
VitalieVSV <dut66AHJCT>
2021-02-21 13:33:28 +0200

    7.14

---
+++ 1/DOM/7.14.js
@@ -0,0 +1,11 @@
+const parent = document.getElementById("parent");
+const child = document.getElementById("child");
+const btn = document.querySelector("button");
+
+const removeBtn = () => {
+    btn.addEventListener("click", () => {
+        parent.removeChild(child);
+    })
+};
+
+//call remove bnt
