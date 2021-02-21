66a631591be1debde0ae566bcb08047a3146616d
VitalieVSV <dut66AHJCT>
2021-02-21 13:28:04 +0200

    7.10

---
+++ 1/DOM/7.10.js
@@ -0,0 +1,4 @@
+// by default it is situated in left corner top
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("ball").style.right = "0%"; // something with top/right
+});
