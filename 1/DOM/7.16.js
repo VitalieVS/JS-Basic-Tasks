78811f8405c7cd61d5c51a71ad2241d49ff8981c
VitalieVSV <dut66AHJCT>
2021-02-21 13:59:29 +0200

    7.16

---
+++ 1/DOM/7.16.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("input");
+
+const inputHandler = () => {
+    input.addEventListener("keyup", () => {
+        input.value =
+            input.value.replace(/[^0-9\.\/]/g, '');
+    })};
+
+inputHandler();
